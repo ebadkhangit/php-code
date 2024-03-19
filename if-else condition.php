35c64c3bd04a1ebb847237118a8a23b4561f4ad2
ebadkhangit <156898309+ebadkhangit@users.noreply.github.com>
2024-03-19 09:20:48 +0530

    Add files via upload

--- if-else condition.php
+++ if-else condition.php
@@ -16,3 +16,25 @@
 											<?php } else {?>
 											<a href="">hhh</a>
 											<?php }?>
+											
+		<div class="row mb-4">
+					<?php
+										$qrymetaltype = "SELECT * FROM product_type order by ptid asc";
+										$exqrymetaltype = custom_my_sql_query($GLOBALS['con'], $qrymetaltype) or die(custom_my_sql_error($GLOBALS['con']));
+										?>	
+						<div class="col-md-4">
+							<div class="input medium " id="stone_type_label">
+								<label for="input1" class="col-form-label">Select Product Type <span class='star'>*</span></label>
+								<div class="input-group" id="product_type_label">
+									<select name="product_type" id="product_type" class="form-select" required="" onchange="manangeCompeteRingSection(this);">
+										<option value="">Select Product Type</option>	
+										<?php while($metalArray = custom_my_sql_fetch_assoc($exqrymetaltype)){?>
+													<option value="<?php echo $metalArray['ptid']?>" <?php if($metalArray['ptid'] == $productResult['PtId']){?> selected="selected" <?php }?>><?php echo $metalArray['ProductType'];?></option>      
+												<?php } ?>
+									</select>
+									<div class="invalid-feedback">This field is required.</div>
+								</div>
+							</div>
+						</div>
+					</div>
+																	
